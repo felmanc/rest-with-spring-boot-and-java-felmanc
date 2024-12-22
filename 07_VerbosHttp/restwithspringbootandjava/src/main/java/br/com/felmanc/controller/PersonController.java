7d78a5e8e4012340d0fc27deb42a16d47ffa907d
felmanc <felmanc@gmail.com>
2024-12-22 20:36:04 -0300

    0806-Refactoring

--- 07_VerbosHttp/restwithspringbootandjava/src/main/java/br/com/felmanc/controller/PersonController.java
+++ 07_VerbosHttp/restwithspringbootandjava/src/main/java/br/com/felmanc/controller/PersonController.java
@@ -1,15 +1,17 @@
 package br.com.felmanc.controller;
 
-
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.felmanc.model.Person;
@@ -20,47 +22,34 @@ import br.com.felmanc.services.PersonServices;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-  
-  @Autowired
-  private PersonServices service;
-  
-  @RequestMapping(method=RequestMethod.GET,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Person> findAll(){
-    return service.findAll();
-  }
 
-  @RequestMapping(value = "/{id}",
-      method=RequestMethod.GET,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  /*@GetMapping(value = "/{id}"
-      produces = MediaTyp.APP)*/
-  public Person findById(
-      @PathVariable(value = "id") Long id)/* throws Exception*/{
-    return service.findById(id);
-  }
+	@Autowired
+	private PersonServices service;
 
-  @RequestMapping(method=RequestMethod.POST,
-      consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person create(
-      @RequestBody Person person)/* throws Exception*/{
-    return service.create(person);
-  }
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
 
-  @RequestMapping(method=RequestMethod.PUT,
-      consumes = MediaType.APPLICATION_JSON_VALUE,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person update(
-      @RequestBody Person person)/* throws Exception*/{
-    return service.update(person);
-  }
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") Long id)/* throws Exception */ {
+		return service.findById(id);
+	}
 
-  @RequestMapping(value = "/{id}",
-      method=RequestMethod.DELETE)
-  public void delete(
-      @PathVariable(value = "id") Long id){
-    service.delete(id);
-  }
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person)/* throws Exception */ {
+		return service.create(person);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person)/* throws Exception */ {
+		return service.update(person);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
