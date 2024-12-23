e6e8d361188b2b191fab6bd6c555665e3f98b97b
felmanc <felmanc@gmail.com>
2024-12-23 16:49:13 -0300

    09_PadraoProjetosVO
    Copia de 09_PadraoProjetosVO

---
+++ 10_VersionamentoAPI/restwithspringbootandjava/src/main/java/br/com/felmanc/controller/PersonController.java
@@ -0,0 +1,56 @@
+package br.com.felmanc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.felmanc.services.PersonServices;
+import br.com.felmanc.vo.v1.PersonVO;
+
+
+//@RestController: retorna um objeto e
+//  o objeto e os dados do objeto são escritos diretamente na resposta HTTP, como JSON ou XML
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById(@PathVariable(/*value = "id"*/) Long id)/* throws Exception */ {
+		return service.findById(id);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create(@RequestBody PersonVO person)/* throws Exception */ {
+		return service.create(person);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update(@RequestBody PersonVO person)/* throws Exception */ {
+		return service.update(person);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
