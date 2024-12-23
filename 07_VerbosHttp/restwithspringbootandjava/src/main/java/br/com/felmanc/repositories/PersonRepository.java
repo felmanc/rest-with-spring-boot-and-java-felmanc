3596797e8e1f74760a5ac0713caa36bbf36f55a7
felmanc <felmanc@gmail.com>
2024-12-22 21:06:00 -0300

    Update 0808

--- 07_VerbosHttp/restwithspringbootandjava/src/main/java/br/com/felmanc/repositories/PersonRepository.java
+++ 07_VerbosHttp/restwithspringbootandjava/src/main/java/br/com/felmanc/repositories/PersonRepository.java
@@ -1,9 +1,9 @@
 package br.com.felmanc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
 import br.com.felmanc.model.Person;
 
-@Repository
+//@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
