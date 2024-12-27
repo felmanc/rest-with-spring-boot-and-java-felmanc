987df09a89dbddc9b6ed94d2f4675857e1b5317d
felmanc <felmanc@gmail.com>
2024-12-27 12:09:30 -0300

    1504 Definindo a Configuração Básica do Swagger (Open API)
    http://localhost:8080/swagger-ui/index.html
    http://localhost:8080/v3/api-docs

--- 15_Swagger/restwithspringbootandjava/src/main/java/br/com/felmanc/services/PersonServices.java
+++ 15_Swagger/restwithspringbootandjava/src/main/java/br/com/felmanc/services/PersonServices.java
@@ -1,11 +1,11 @@
 package br.com.felmanc.services;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 import java.util.logging.Logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.stereotype.Service;
 
 import br.com.felmanc.controller.PersonController;
