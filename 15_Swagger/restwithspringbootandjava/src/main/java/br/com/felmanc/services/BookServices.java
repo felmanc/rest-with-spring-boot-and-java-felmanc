f25d4434d02bedde39fd6f95364b5848cf9009bc
felmanc <felmanc@gmail.com>
2024-12-28 22:06:57 -0300

    1508 [DESAFIO] Crie Testes Unitários com Mockito para Validar HATEOAS em Books
    Adicionado HATEOAS e Swagger

--- 15_Swagger/restwithspringbootandjava/src/main/java/br/com/felmanc/services/BookServices.java
+++ 15_Swagger/restwithspringbootandjava/src/main/java/br/com/felmanc/services/BookServices.java
@@ -1,11 +1,14 @@
 package br.com.felmanc.services;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 import java.util.logging.Logger;
 
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.felmanc.controller.BookController;
 import br.com.felmanc.data.vo.v1.BookVO;
 import br.com.felmanc.exceptions.RequiredObjectIsNullException;
 import br.com.felmanc.exceptions.ResourceNotFoundException;
@@ -13,7 +16,7 @@ import br.com.felmanc.mapper.DozerMapper;
 import br.com.felmanc.model.Book;
 import br.com.felmanc.repositories.BookRepository;
 
-//TODO: Implementar o HATEOAS!
+
 @Service
 public class BookServices {
 	private Logger logger = Logger.getLogger(BookServices.class.getName());
@@ -23,13 +26,16 @@ public class BookServices {
 	public BookServices(BookRepository repository) {
 		this.repository = repository;
 	}
-	
-	
+
 	public List<BookVO> findAll() {
 		logger.info("Find all books!");
 		
 		var books = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
-		
+
+		books
+			.stream()
+			.forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+
 		return books;
 	}
 	
@@ -41,9 +47,11 @@ public class BookServices {
 		
 		var vo = DozerMapper.parseObject(entity, BookVO.class);
 		
+		vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		
 		return vo;
 	}
-	
+
 	public BookVO create(BookVO bookVO) {
 		
 		if(bookVO == null) throw new RequiredObjectIsNullException();
@@ -53,6 +61,8 @@ public class BookServices {
 		var entity = DozerMapper.parseObject(bookVO, Book.class);
 		var vo = DozerMapper.parseObject(repository.save(entity), BookVO.class);
 		
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+
 		return vo;
 	}
 	
@@ -60,6 +70,9 @@ public class BookServices {
 		
 		if(bookVO == null) throw new RequiredObjectIsNullException();
 		
+		logger.info("Updating one book!");
+
+		// BookVO
 		var entity = repository.findById(bookVO.getKey())
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID: " + bookVO.getKey()));
 				
@@ -71,6 +84,8 @@ public class BookServices {
 
 		var vo = DozerMapper.parseObject(repository.save(entity), BookVO.class);
 		
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+
 		return vo;
 	}
 	
