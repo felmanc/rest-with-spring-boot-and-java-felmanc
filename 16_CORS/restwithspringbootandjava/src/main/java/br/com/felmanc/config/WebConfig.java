89e66334c02b15b52cddc116ab505f7babce58c5
felmanc <felmanc@gmail.com>
2024-12-31 12:41:51 -0300

    1604 Habilitando CORS de Forma Global

--- 16_CORS/restwithspringbootandjava/src/main/java/br/com/felmanc/config/WebConfig.java
+++ 16_CORS/restwithspringbootandjava/src/main/java/br/com/felmanc/config/WebConfig.java
@@ -2,14 +2,29 @@ package br.com.felmanc.config;
 
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration: Indica ao SpringBoot ao carregar a aplicação que deve ler esta classe que contém o comportamento da aplicação
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+			//.allowedMethods("GET", "POST", "PUT")
+			.allowedMethods("*")
+			.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+	}
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
