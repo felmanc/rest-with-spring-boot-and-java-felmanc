5899480690f7b51d87e36aea9c995c0e3c12480a
felmanc <felmanc@gmail.com>
2025-01-16 14:42:04 -0300

    2103 Definindo as Exceptions e Response

--- 21_WorkingWithFiles/restwithspringbootandjava/src/main/java/br/com/felmanc/exceptions/MyFileNotFoundException.java
+++ 21_WorkingWithFiles/restwithspringbootandjava/src/main/java/br/com/felmanc/exceptions/MyFileNotFoundException.java
@@ -11,11 +11,10 @@ import br.com.felmanc.repositories.PersonRepository;
 public class MyFileNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	@Autowired
-	PersonRepository repository;
-	
 	public MyFileNotFoundException(String ex) {
 		super(ex);
 	}
 
+	public MyFileNotFoundException(String ex, Throwable cause) {
+		super(ex, cause);
 }
