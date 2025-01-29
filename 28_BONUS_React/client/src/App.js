533dacac55911c5ca6e377c025ab9d9bbd598ad2
felmanc <felmanc@gmail.com>
2025-01-29 10:24:47 -0300

    2811 Iniciando a Implementação do Login

--- 28_BONUS_React/client/src/App.js
+++ 28_BONUS_React/client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './global.css';
+
+import Login from './pages/Login';
+
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <Login/>
   );
 }
