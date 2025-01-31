4f1b1cd4adcb433e3550f8586b115f149b7aa16d
felmanc <felmanc@gmail.com>
2025-01-31 17:19:36 -0300

    2818 Implementando a Listagem da Tela de Livros

--- 28_BONUS_React/client/src/pages/Book/index.js
+++ 28_BONUS_React/client/src/pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -17,6 +17,108 @@ export default function Books() {
                     <FiPower size={18} color="#251FC5" />
                 </button>
             </header>
+
+            <h1>Registered Books</h1>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,90</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5"/>
+                    </button>
+                </li>
+            </ul>
         </div>
     );
 }
